f2552000ac238491c43cd9d4f798355ef9de470f
PAULO A. CARMONA Z <pcarmonaz@gmail.com>
2021-11-25 23:53:30 -0500

    Create order specs and edit the model to make pass its tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,31 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :delete_all
+  accepts_nested_attributes_for :line_items
+  
+  validates :subtotal, :tax, :total, presence: true
+
+  before_validation :calc_subtotal, :calc_tax, :calc_total
+
+  TAX_ORDER = 0.08.freeze
+
+  def recalculate!
+    calc_subtotal
+    calc_tax
+    calc_total
+    save!
+  end
+
+  protected
+
+  def calc_subtotal
+    self.subtotal = line_items.map {|li| li.quantity.to_i * li.amount.to_i }.reduce(&:+).to_i
+  end
+
+  def calc_tax
+    self.tax = self.subtotal * TAX_ORDER
+  end
+
+  def calc_total
+    self.total = self.subtotal + self.tax
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { described_class.new }
+
+  context 'When it is instantiated' do
+    it 'has a subtotal' do
+      expect(subject.subtotal).to be_nil
+    end
+
+    it 'has a tax' do
+      expect(subject.tax).to be_nil
+    end
+
+    it 'has a total' do
+      expect(subject.total).to be_nil
+    end
+  end
+
+  context 'when it is saved' do
+    before { subject.save }
+
+    it 'has a subtotal equal to 0' do
+      expect(subject.subtotal).to eq(0)
+    end
+
+    it 'has a tax equal to 0.0' do
+      expect(subject.tax).to eq(0.0)
+    end
+
+    it 'has a total equal to 0.0' do
+      expect(subject.total).to eq(0.0)
+    end
+  end
 end
